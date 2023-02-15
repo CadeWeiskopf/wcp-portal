f5c5753947289cd22e00a61818e4224da493dc82
CadeWeiskopf <weiskopfcd@gmail.com>
2023-02-15 10:14:32 -0500

    apiRequester in context

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { useContext, useEffect } from "react";
 import "./App.css";
 import CheckOutForm from "./components/CheckOutForm";
-import { AppContextProvider } from "./tools/AppContext";
+import AppContext, { AppContextProvider } from "./tools/AppContext";
 
 function App() {
+  const { loadingMessage, setLoadingMessage, isLoading, setIsLoading } =
+    useContext(AppContext);
+  useEffect(() => {
+    setLoadingMessage("test");
+  }, []);
   return (
     <div className="App">
       <CheckOutForm />
--- src/tools/AppContext.tsx
+++ src/tools/AppContext.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
+import { ApiRequester } from "./ApiRequester";
 
 interface AppContextProps {
   isLoading: boolean;
   setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
   loadingMessage: string;
   setLoadingMessage: React.Dispatch<React.SetStateAction<string>>;
+  apiRequester: ApiRequester;
 }
 
 const AppContext = React.createContext<AppContextProps>({
@@ -12,14 +14,22 @@ const AppContext = React.createContext<AppContextProps>({
   setIsLoading: () => {},
   loadingMessage: "",
   setLoadingMessage: () => {},
+  apiRequester: new ApiRequester(),
 });
 
 function AppContextProvider(props: any) {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [loadingMessage, setLoadingMessage] = useState<string>("");
+  const [apiRequester] = useState<ApiRequester>(new ApiRequester());
   return (
     <AppContext.Provider
-      value={{ isLoading, setIsLoading, loadingMessage, setLoadingMessage }}
+      value={{
+        isLoading,
+        setIsLoading,
+        loadingMessage,
+        setLoadingMessage,
+        apiRequester,
+      }}
     >
       {props.children}
     </AppContext.Provider>
