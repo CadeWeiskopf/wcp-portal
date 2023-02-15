08f1d3f88a0218539f615be853de92c573d49540
CadeWeiskopf <weiskopfcd@gmail.com>
2023-02-15 12:35:38 -0500

    rm create react app stuff

--- src/tools/ApiRequester.ts
+++ src/tools/ApiRequester.ts
@@ -1,7 +1,15 @@
 export class ApiRequester {
-  constructor() {}
+  constructor() {
+    if (!process.env.REACT_APP_API_URL) {
+      throw Error("Missing environment variables");
+    } else {
+      console.log(process.env.REACT_APP_API_URL);
+    }
+  }
 
   async getData() {
     console.log("getData");
+    const response = await fetch(`${process.env.REACT_APP_API_URL}`);
+    console.log(response);
   }
 }
